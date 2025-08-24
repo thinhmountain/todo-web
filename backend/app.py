7da3be6199c9a6604cd1899b6c8ffbd68c0dd4fd
ThinhMountain <Thinhmountain2511@gmail.com>
2025-08-24 14:25:55 +0700

    add

--- backend/app.py
+++ backend/app.py
@@ -4,16 +4,16 @@ from flask import Flask,render_template,send_file,url_for
 
 
 app = create_app()    
+
+app.register_blueprint(create)
+app.register_blueprint(read)
+app.register_blueprint(update)
+app.register_blueprint(delete)
+app.register_blueprint(login)
+app.register_blueprint(logout)
+app.register_blueprint(register)
+app.register_blueprint(todo)
+
 if __name__ == "__main__":
-
-    app.register_blueprint(create)
-    app.register_blueprint(read)
-    app.register_blueprint(update)
-    app.register_blueprint(delete)
-    app.register_blueprint(login)
-    app.register_blueprint(logout)
-    app.register_blueprint(register)
-    app.register_blueprint(todo)
-
     app.run(debug=True)
 
